e3942949ab3d556a1590ef45e271345438bf7ef3
jiangxuefei1200 <jiangxuefei1200@163.com>
2025-09-27 09:48:11 +0800

    测试修改文件hello4.py

--- hello4.py
+++ hello4.py
@@ -3,6 +3,7 @@ def say_hello():
     print("Hello, GitHub!")
     print("Hello, jiangxuefei1200!")
     print("Hello, jiangxuefei1200!")
+    print("Hello, jiangxuefei1200!")
 
 if __name__ == "__main__":
     say_hello()
