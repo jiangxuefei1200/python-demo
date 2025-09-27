acc0de9da16d82f34cd0248968ad7ce8f9090965
jiangxuefei1200 <jiangxuefei1200@163.com>
2025-09-27 11:02:17 +0800

    Update hello4.py

--- hello4.py
+++ hello4.py
@@ -5,6 +5,7 @@ def say_hello():
     print("Hello, jiangxuefei1200!")
     print("Hello, jiangxuefei1200!")
     print("Hello, jiangxuefei1200!")
+    print("Hello, jiangxuefei1200! edited in github.")
 
 if __name__ == "__main__":
     say_hello()
