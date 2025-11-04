ad202ee2cc2d82580f7371e2d4384979ff4c8006
jiangxuefei1200 <jiangxuefei1200@163.com>
2025-11-04 11:45:32 +0800

    在develop/branch1分支上添加函数

--- hello.py
+++ hello.py
@@ -2,6 +2,10 @@
 def say_hello():
     print("Hello, GitHub!")
 
+#在develop/branch1添加方法
+def say_hello_branch1():
+    print("Hello, Branch1!")
+
 #在develop/branch1上测试修改
 if __name__ == "__main__":
     say_hello()
